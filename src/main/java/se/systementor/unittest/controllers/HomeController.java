cd1ab8610bc4adca634173f681d5e6b8eb7061aa
Stefan Holmberg <stefan.holmberg@systementor.se>
2025-05-22 20:10:31 +0200

    Update HomeController.java

--- src/main/java/se/systementor/unittest/controllers/HomeController.java
+++ src/main/java/se/systementor/unittest/controllers/HomeController.java
@@ -1,4 +1,33 @@
 package se.systementor.unittest.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    String home(Model model) {
+        model.addAttribute("age", 18);
+        model.addAttribute("location", "K");
+        model.addAttribute("promille", 0.0);
+        return "home";
+    }
+
+
+    @PostMapping("/")
+    String homePost(Model model, int age, String location, float promille ) {
+        model.addAttribute("age", age);
+        model.addAttribute("location", location);
+        model.addAttribute("promille", promille);
+
+
+
+
+        return "home";
+    }
+
 }
